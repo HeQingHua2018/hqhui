b4e4af7df4c48e8bd9fa72a13ec88e0437f40ea4
何青华 <heqinghua@heqinghuadeMacBook-Pro.local>
2025-04-14 11:20:02 +0800

    chore: 修改配置文件

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,32 +1,30 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',  // 默认静态资源输出目录
-  base: '/hqhui/',        // 若仓库名为 username.github.io，则设为 '/'
-  publicPath: '/hqhui/',  // 资源路径前缀，与 base 一致
+  outputPath: 'docs-dist', // 默认静态资源输出目录
+  base: '/hqhui/', // 若仓库名为 username.github.io，则设为 '/'
+  publicPath: '/hqhui/', // 资源路径前缀，与 base 一致
   themeConfig: {
     name: 'HQHUI',
     editLink: false,
     // lastUpdated: false,
-    logo:false,
+    logo: false,
     footer: false,
-    locales: [{id:'zh-CN', name:'简体中文'}],
-    nav:[
-      {title:'使用指南', link:'/guide'},
-      {title:'组件', link:'/components'},
-      {title:'工具', link:'/utils'},
-      {title:'更新日志',link:'/changelogs'}
+    locales: [{ id: 'zh-CN', name: '简体中文' }],
+    nav: [
+      { title: '使用指南', link: '/guide' },
+      { title: '组件', link: '/components' },
+      { title: '工具', link: '/utils' },
+      { title: '更新日志', link: '/changelogs' },
     ],
-    metas:[],
+    metas: [],
   },
-  apiParser: {
-  },
-  resolve:{
+  apiParser: {},
+  resolve: {
     entryFile: './src/index.ts',
-  
     atomDirs: [
       { type: 'component', dir: 'src/components' },
       { type: 'util', dir: 'src/utils' },
     ],
-  }
+  },
 });
