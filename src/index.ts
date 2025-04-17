30fcb30a521c3621812e8765b6d5bee26eda0ff3
何青华 <heqinghua@heqinghuadeMacBook-Pro.local>
2025-04-17 11:15:23 +0800

    chore: package.json 修改

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,28 @@ export { default as PrimaryButton } from './components/PrimaryButton';
  * 工具库
  */
 
+export { generateRandomString, getUUID } from './utils/chart';
 export { default as formatTime } from './utils/formatTime';
+export {
+  findNode,
+  findNodeByValue,
+  flatTreeData,
+  getParent,
+  getParentByValue,
+  list2Tree,
+  loopData,
+} from './utils/tree';
+export {
+  isEmail,
+  isFalse,
+  isGeneral,
+  isIdNumber,
+  isIncludeHtml,
+  isMatchRegex,
+  isNumber,
+  isPhone,
+  isPositiveInteger,
+  isPositiveIntegerAndZero,
+  isTrue,
+  isUrl,
+} from './utils/validation';
